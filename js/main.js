f0cbd6696eec2c89ff552bb68623c9f456942c94
marenkol <marenkolilia@gmail.com>
2016-04-27 14:18:37 +0300

    flex

---
+++ js/main.js
@@ -0,0 +1,13 @@
+;(function(){
+	var body = document.body,
+		btn = body.querySelector('.ba-toggle'),
+		overlay = body.querySelector('.ba-overlay');
+
+		console.log(btn);
+		btn.onclick = function(){
+			body.classList.toggle('ba-menu-open');
+		}
+		overlay.onclick = function(){
+			body.classList.toggle('ba-menu-open');
+		}
+})();
